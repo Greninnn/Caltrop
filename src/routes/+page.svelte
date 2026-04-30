0da78cefd28e9367f6ee94e28806612ad8501285
Greninnn <alexjaejoon@gmail.com>
2026-04-30 14:03:57 -0500

    removed some default tauri stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,58 +17,27 @@
         // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
         greetMsg = await invoke("get_image_for_id", { api_key });
     }
+    async function getImage(id: String): Promise<String> {
+        return await invoke("get_image_for_id", { id }).then((image) => image.url)
+    } 
 </script>
 
 <main class="container">
-    <h1>Welcome to Tauri + Svelte</h1>
-
-    <div class="row">
-        <a href="https://vite.dev" target="_blank">
-            <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-            <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-        </a>
-        <a href="https://svelte.dev" target="_blank">
-            <img
-                src="/svelte.svg"
-                class="logo svelte-kit"
-                alt="SvelteKit Logo"
-            />
-        </a>
-    </div>
-    <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
-
-    <form class="row" onsubmit={greet}>
-        <input
-            id="greet-input"
-            placeholder="Enter a name..."
-            bind:value={name}
-        />
-        <button type="submit">Greet</button>
-    </form>
+    <h1>Welcome to coolapplauncher</h1>
 
     <form class="row" onsubmit={setApiKey}>
         <input
             id="api-input"
-            placeholder="Enter api key..."
+            placeholder="Enter steamdb api key"
             bind:value={api_key}
         />
-        <button type="submit">Greet</button>
+        <button type="submit">Submit</button>
     </form>
 
     <p>{greetMsg}</p>
-    <p>{kim}</p>
 </main>
 
 <style>
-    .logo.vite:hover {
-        filter: drop-shadow(0 0 2em #747bff);
-    }
-
-    .logo.svelte-kit:hover {
-        filter: drop-shadow(0 0 2em #ff3e00);
-    }
 
     :root {
         font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
@@ -77,7 +46,7 @@
         font-weight: 400;
 
         color: #0f0f0f;
-        background-color: #f6f6f6;
+        background-color: #a5b4a2b6;
 
         font-synthesis: none;
         text-rendering: optimizeLegibility;
@@ -95,17 +64,6 @@
         text-align: center;
     }
 
-    .logo {
-        height: 6em;
-        padding: 1.5em;
-        will-change: filter;
-        transition: 0.75s;
-    }
-
-    .logo.tauri:hover {
-        filter: drop-shadow(0 0 2em #24c8db);
-    }
-
     .row {
         display: flex;
         justify-content: center;
@@ -115,10 +73,6 @@
         font-weight: 500;
         color: #646cff;
         text-decoration: inherit;
-    }
-
-    a:hover {
-        color: #535bf2;
     }
 
     h1 {
@@ -156,10 +110,6 @@
         outline: none;
     }
 
-    #greet-input {
-        margin-right: 5px;
-    }
-
     #api-input {
         margin-right: 5px;
     }
@@ -168,10 +118,6 @@
         :root {
             color: #f6f6f6;
             background-color: #2f2f2f;
-        }
-
-        a:hover {
-            color: #24c8db;
         }
 
         input,
